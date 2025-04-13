df9029131fe71596a5aa0d39ef03c64aa084cde3
noobexon1 <noamf92@gmail.com>
2025-04-13 19:47:07 +0300

    Refactor MapViewContainer to disable built-in zoom controls for improved UI experience

--- app/src/main/java/com/noobexon/xposedfakelocation/manager/ui/map/components/MapViewContainer.kt
+++ app/src/main/java/com/noobexon/xposedfakelocation/manager/ui/map/components/MapViewContainer.kt
@@ -97,7 +97,7 @@ private fun rememberMapView(context: Context): MapView {
     return remember {
         MapView(context).apply {
             setTileSource(TileSourceFactory.MAPNIK)
-            setZoomButtonsVisible(false)
+            setBuiltInZoomControls(false)
             setMultiTouchControls(true)
         }
     }
